332a228e8a3ea09a285d4d03fa253dbb928961f6
Maikomi <58994228+Maikomi@users.noreply.github.com>
2023-11-21 01:36:19 +0100

    coraz bliżej końca

--- matrix.js
+++ matrix.js
@@ -199,7 +199,6 @@ const transposeMatrix = (m1) => {
     return result;
 }
 
-//Prawie dobrze, ale nadal źle. Minusy są nie tak
 const inverseMatrix = (m1) => {
     let n = m1.getSize();
     if (n < 2) {
@@ -223,6 +222,126 @@ const inverseMatrix = (m1) => {
     return transposeMatrix(result);
 }
 
+//translacja macierzy
+
+const translateMatrix = (m1, dx, dy, dz) => {
+    const n = m1.getSize();
+
+    if (n === 3) {
+        // Translacja dla macierzy 3x3
+        const translationMatrix = new SqrMatrix(3);
+        translationMatrix.setValue(0, 0, 1);
+        translationMatrix.setValue(1, 1, 1);
+        translationMatrix.setValue(2, 2, 1);
+        translationMatrix.setValue(0, 2, dx);
+        translationMatrix.setValue(1, 2, dy);
+
+        return multiplyMatrices(m1, translationMatrix);
+    } else if (n === 4) {
+        // Translacja dla macierzy 4x4 (3D)
+        const translationMatrix = new SqrMatrix(4);
+        translationMatrix.setValue(0, 0, 1);
+        translationMatrix.setValue(1, 1, 1);
+        translationMatrix.setValue(2, 2, 1);
+        translationMatrix.setValue(3, 3, 1);
+        translationMatrix.setValue(0, 3, dx);
+        translationMatrix.setValue(1, 3, dy);
+        translationMatrix.setValue(2, 3, dz);
+
+        return multiplyMatrices(m1, translationMatrix);
+    } else {
+        console.log("Translacja dostępna tylko dla macierzy 3x3 i 4x4");
+        return null;
+    }
+}
+
+//Skalowanie macierzy
+
+const scaleMatrix = (m1, scaleX, scaleY, scaleZ) => {
+    const n = m1.getSize();
+
+    if (n === 3) {
+        // Skalowanie dla macierzy 3x3
+        const scalingMatrix = new SqrMatrix(3);
+        scalingMatrix.setValue(0, 0, scaleX);
+        scalingMatrix.setValue(1, 1, scaleY);
+        scalingMatrix.setValue(2, 2, scaleZ);
+
+        return multiplyMatrices(m1, scalingMatrix);
+    } else if (n === 4) {
+        // Skalowanie dla macierzy 4x4 (3D)
+        const scalingMatrix = new SqrMatrix(4);
+        scalingMatrix.setValue(0, 0, scaleX);
+        scalingMatrix.setValue(1, 1, scaleY);
+        scalingMatrix.setValue(2, 2, scaleZ);
+        scalingMatrix.setValue(3, 3, 1);
+
+        return multiplyMatrices(m1, scalingMatrix);
+    } else {
+        console.log("Skalowanie dostępne tylko dla macierzy 3x3 i 4x4");
+        return null;
+    }
+}
+
+//Obroty macierzy
+const rotateMatrix = (m1, angleX, angleY, angleZ) => {
+    const n = m1.getSize();
+
+    if (n === 3) {
+        // Obracanie macierzy 3x3 wokół osi Z
+        const rotationMatrix = new SqrMatrix(3);
+        const cosZ = Math.cos(angleZ);
+        const sinZ = Math.sin(angleZ);
+
+        rotationMatrix.setValue(0, 0, cosZ);
+        rotationMatrix.setValue(0, 1, -sinZ);
+        rotationMatrix.setValue(1, 0, sinZ);
+        rotationMatrix.setValue(1, 1, cosZ);
+        rotationMatrix.setValue(2, 2, 1);
+
+        return multiplyMatrices(m1, rotationMatrix);
+    } else if (n === 4) {
+        // Obracanie macierzy 4x4 (3D) wokół osi X, Y, Z
+        const rotationX = new SqrMatrix(4);
+        const rotationY = new SqrMatrix(4);
+        const rotationZ = new SqrMatrix(4);
+
+        const cosX = Math.cos(angleX);
+        const sinX = Math.sin(angleX);
+        const cosY = Math.cos(angleY);
+        const sinY = Math.sin(angleY);
+        const cosZ = Math.cos(angleZ);
+        const sinZ = Math.sin(angleZ);
+
+        rotationX.setValue(0, 0, 1);
+        rotationX.setValue(1, 1, cosX);
+        rotationX.setValue(1, 2, -sinX);
+        rotationX.setValue(2, 1, sinX);
+        rotationX.setValue(2, 2, cosX);
+        rotationX.setValue(3, 3, 1);
+
+        rotationY.setValue(0, 0, cosY);
+        rotationY.setValue(0, 2, sinY);
+        rotationY.setValue(1, 1, 1);
+        rotationY.setValue(2, 0, -sinY);
+        rotationY.setValue(2, 2, cosY);
+        rotationY.setValue(3, 3, 1);
+
+        rotationZ.setValue(0, 0, cosZ);
+        rotationZ.setValue(0, 1, -sinZ);
+        rotationZ.setValue(1, 0, sinZ);
+        rotationZ.setValue(1, 1, cosZ);
+        rotationZ.setValue(2, 2, 1);
+        rotationZ.setValue(3, 3, 1);
+
+        const combinedRotation = multiplyMatrices(rotationX, multiplyMatrices(rotationY, rotationZ));
+
+        return multiplyMatrices(m1, combinedRotation);
+    } else {
+        console.log("Obracanie dostępne tylko dla macierzy 3x3 i 4x4");
+        return null;
+    }
+}
 
 //testowanie
 
@@ -321,3 +440,94 @@ console.log("macierz odwrotna mała");
 m3 = inverseMatrix(m4);
 m3.print();
 
+console.log("Translacja macierzy");
+const m6 = new SqrMatrix(4);
+
+m6.setValue(0, 0, 1);
+m6.setValue(0, 1, 2);
+m6.setValue(0, 2, 3);
+m6.setValue(0, 3, 4);
+m6.setValue(1, 0, 5);
+m6.setValue(1, 1, 6);
+m6.setValue(1, 2, 7);
+m6.setValue(1, 3, 8);
+m6.setValue(2, 0, 9);
+m6.setValue(2, 1, 10);
+m6.setValue(2, 2, 11);
+m6.setValue(2, 3, 12);
+m6.setValue(3, 0, 13);
+m6.setValue(3, 1, 14);
+m6.setValue(3, 2, 15);
+m6.setValue(3, 3, 16);
+
+console.log("Macierz przed translacją:");
+m6.print();
+
+// Przesunięcie o wektor (2, 3, 1)
+const translatedMatrix2 = translateMatrix(m6,2,3,1);
+
+console.log("Macierz po translacji:");
+translatedMatrix2.print();
+
+const matrixToScale = new SqrMatrix(3);
+
+matrixToScale.setValue(0, 0, 1);
+matrixToScale.setValue(0, 1, 2);
+matrixToScale.setValue(0, 2, 3);
+matrixToScale.setValue(1, 0, 4);
+matrixToScale.setValue(1, 1, 5);
+matrixToScale.setValue(1, 2, 6);
+matrixToScale.setValue(2, 0, 7);
+matrixToScale.setValue(2, 1, 8);
+matrixToScale.setValue(2, 2, 9);
+
+console.log("Macierz przed skalowaniem:");
+matrixToScale.print();
+
+// Skalowanie o wartości 2 wzdłuż osi x, 3 wzdłuż osi y, i 0.5 wzdłuż osi z
+const scaledMatrix = scaleMatrix(matrixToScale, 2, 3, 0.5);
+
+console.log("Macierz po skalowaniu:");
+scaledMatrix.print();
+
+console.log("Obracanie macierzy");
+const m7 = new SqrMatrix(3);
+
+m7.setValue(0, 0, 1);
+m7.setValue(0, 1, 2);
+m7.setValue(0, 2, 3);
+m7.setValue(1, 0, 4);
+m7.setValue(1, 1, 5);
+m7.setValue(1, 2, 6);
+m7.setValue(2, 0, 7);
+m7.setValue(2, 1, 8);
+m7.setValue(2, 2, 9);
+
+console.log("Macierz przed obrotem:");
+m7.print();
+
+// Obrót wokół osi Z (kąt 45 stopni)
+const rotatedMatrixZ = rotateMatrix(m7, 0, 0, Math.PI / 4);
+
+console.log("Macierz po obrocie wokół osi Z:");
+rotatedMatrixZ.print();
+
+// Obrót wokół osi X (kąt 90 stopni)
+const rotatedMatrixX = rotateMatrix(m7, Math.PI / 2, 0, 0);
+
+console.log("Macierz po obrocie wokół osi X:");
+rotatedMatrixX.print();
+
+// Obrót wokół osi Y (kąt 60 stopni)
+const rotatedMatrixY = rotateMatrix(m7, 0, Math.PI / 3, 0);
+
+console.log("Macierz po obrocie wokół osi Y:");
+rotatedMatrixY.print();
+
+
+//Zadanie
+console.log("");
+console.log("");
+console.log("");
+console.log("Zadanie sprawdzające");
+console.log("Wektor [1, 0, 0, 1] i obrót o 90 stopni wokół osi Y");
